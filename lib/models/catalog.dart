bb6d462cf21d1daa9c5d1e73b07667ce0e5ed5c2
Mustansar <mustansar.mh30@gmail.com>
2022-02-01 20:23:11 +0500

    day 10 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,12 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final int price;
+  final String color;
+  final String image;
+
+  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+}
+
+final product = [Item("1", "iphone", "made by apple", 999, "red", "image")];
